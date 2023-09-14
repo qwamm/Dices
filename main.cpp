6b11c00b64b0587cb925f9e6411dbba0c811a63e
qwamm <anonimmail123@bk.ru>
2023-09-14 23:42:44 +0300

    14.09.23

--- main.cpp
+++ main.cpp
@@ -3,8 +3,99 @@
 
 using namespace simple_class;
 
+int invite();
+int D_Init (Dice &d);
+int D_Random_Value (Dice &d);
+int D_Generation_based_on_probabilities (Dice &d);
+int D_Print (Dice &d);
+
+int (*f[])(Dice&) = {NULL, D_Init, D_Random_Value, D_Generation_based_on_probabilities};
+
 int main()
 {
-    Dice a;
-    a.print();
+    int rc;
+    Dice d;
+    while (rc = invite())
+	if (!f[rc](d))
+		break;
+    D_Print(d);
+    std::cout<<"That's all. Good luck!\n";
+}
+
+int D_Print (Dice &d)
+{
+	try
+	{
+		d.print();
+	}
+	catch(...)
+	{
+		throw;
+		return 0;
+	}
+	return 1;
+}
+
+int D_Generation_based_on_probabilities (Dice &d)
+{
+	try
+	{
+		d.generate_random_value();
+	}
+	catch(...)
+	{
+		throw;
+		return 0;
+	}
+	return 1;
+}
+
+int D_Random_Value (Dice &d)
+{
+	try
+	{
+	 	d.set_random();
+	}
+	catch(...)
+	{
+		throw;
+		return 0;
+	}
+	return 1;
+}
+
+int D_Init (Dice &d)
+{
+	try
+	{
+		d.set_state();
+	}
+	catch(...)
+	{
+		throw;
+		return 0;
+	}
+	return 1;
+}
+
+int invite()
+{
+        std::cout << "Select one option:\n";
+        std::cout << " 0. Quit\n";
+        std::cout << " 1. Initialization with the side dropped out and probabilities of dropping sides:\n";
+        std::cout << " 2. Initialization with the random side\n";
+        std::cout << " 3. Generating a random value for a dice based on probability\n";
+        std::cout << " 4. Compare two dices\n";
+        std::cout << " 5. ASCII-art\n";
+	int rc;
+	try
+	{
+        	rc = getNum<int>(0,3);
+	}
+	catch(...)
+	{
+		throw;
+		return 0;
+	}
+        return rc;
 }
--- simple_class.h
+++ simple_class.h
@@ -2,12 +2,36 @@
 #define LAB2_SIMPLE_CLASS_H
 
 #include <iostream>
+#include <cstring>
 #include <cstdlib>
 #include <cmath>
-#include <random>
+#include <algorithm>
+#include <functional>
 
 namespace simple_class
 {
+    template <class T>
+    T getNum(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max())
+    {
+        T a;
+        while (true)
+        {
+            std:: cin >> a;
+            if (std::cin.eof()) //конец файла
+                throw std::runtime_error("Failed to read number: EOF");
+            else if (std::cin.bad()) //невосстановимая ошибка входного потока
+                throw  std::runtime_error(std::string("Failed to read number") + strerror(errno));
+            else if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std:: cout<<"You are wrong, repeat please!"<<std::endl;
+            }
+            else if (a>=min && a<=max)
+                return a;
+        }
+    }
+
+
     class Dice
     {
     private:
@@ -56,9 +80,32 @@ namespace simple_class
             }
         }
 
-        void generate_random_value()
+        void generate_random_value() //генерация рандомного значения игральной кости с учетом вероятностей
         {
             srand(time(nullptr));
+	    int p = rand()%101;
+	    double cnt = (int) (this->probabilities[0]*100);
+	    //std::copy(this->probabilities, this->probabilities + 6, buf);
+	    //std::sort(buf, buf + 6, std::greater<double>());
+	   /*for (int i=0; i<6; i++)
+	   {
+		std::cout<<buf[i]<<" ";
+	   }
+	   std::cout<<std::endl; */
+	    for (int i = 0; i<6; i++)
+	    {
+		if (this->probabilities[i] != 0 && cnt > p)
+		{
+			this->value = i + 1;
+			break;
+		}
+		else if (i < 5)
+		{
+			cnt += (int) (this->probabilities[i+1]*100);
+		}
+	    }
+	    /*delete[] buf;
+	    buf = nullptr;*/
         }
 
         double get_probability(int num) //изменение значения вероятности выпадения числа
@@ -68,13 +115,17 @@ namespace simple_class
 
         void set_state() //ввод состояния класса через входной поток
         {
-            std::cout << "Enter current value of dice: \n";
-            std::cin >> this->value;
-            for (int i=0; i<6; i++)
-            {
-                std::cout << "Enter probability of dropping " << (i+1) << " number:\n";
-                std::cin >> this->probabilities[i];
-            }
+            	std::cout << "Enter dropped value of dice:\n";
+        	int val = getNum<int>(1,6);
+        	this->value = val;
+        	double sum = 1.0;
+        	for (int i=0; i<6; i++)
+        	{
+                	std::cout << "Enter probability of dropping number #" << (i+1) << ":\n";
+                	double cur = getNum<double>(0,sum);
+			this->probabilities[i] = cur;
+                	sum -= cur;
+        	}
         }
 
         void print() //вывод состояния игральной кости
@@ -86,6 +137,11 @@ namespace simple_class
             }
         }
     };
+
+   //int invite();
+   //int D_Create(Dice &d);
+
+   //void D_Create();
 }
 
 #endif //LAB2_SIMPLE_CLASS_H
